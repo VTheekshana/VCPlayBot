7a35680cb6da52a3fbae5bc8a142d1763353080f
Vinu Theekshana <87488227+VTheekshana@users.noreply.github.com>
2021-07-31 19:57:05 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -32,7 +32,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Finding the song @TheMizuBot...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -55,13 +55,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit("Downloading the song @TheMizuBot ")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @vc_play_slbotzone_bot **'
+        rep = '**🎵 Uploaded by @TheMizuBot **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -259,7 +259,7 @@ async def jssong(_, message):
     is_downloading = True
     text = message.text.split(None, 1)[1]
     query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching...")
+    m = await message.reply_text("Searching By @TheMizuBot...")
     try:
         songs = await arq.saavn(query)
         sname = songs[0].song
